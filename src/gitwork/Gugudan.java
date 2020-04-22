d0ff40d3c0a4112357a9bdaa390dfa9fca5030d0
ygw <inchrist95@naver.com>
2020-04-22 22:48:50 +0900

    zero function added

--- src/gitwork/Gugudan.java
+++ src/gitwork/Gugudan.java
@@ -6,6 +6,15 @@ public class Gugudan {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		if(n==0) {
+			for (int i = 2; i < 10; i++) {
+				for (int j = 1; j < 10; j++) {
+					System.out.print(i+"*"+j+"="+i*j+"\t");
+				}
+				System.out.println();
+			}
+			return;
+		}
 		for (int i = 1; i < 10; i++) {
 			System.out.println(n+"*"+i+"="+n*i);
 		}
