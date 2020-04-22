9f01ab4a98f3b29c6a44e68f93390190e86fa7fe
ygw <inchrist95@naver.com>
2020-04-23 00:09:25 +0900

    added pie.java and readme.md

---
+++ src/gitwork/Pie.java
@@ -0,0 +1,13 @@
+package gitwork;
+
+public class Pie {
+	public static void main(String[] args) {
+		double result = 0;
+		for (int i = 1; i < 10000000; i++) {
+			double n = (double)1/(2*i-1);
+			if(i%2==0) n*=-1;
+			result += n;
+		}
+		System.out.println("ÆÄÀÌ / 4 = "+result);
+	}
+}
