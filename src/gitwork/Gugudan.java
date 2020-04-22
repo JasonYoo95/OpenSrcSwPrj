5b65b546c50f8cc3361a8de056e89544da8a74f6
ygw <inchrist95@naver.com>
2020-04-22 22:57:17 +0900

    negative function added

--- src/gitwork/Gugudan.java
+++ src/gitwork/Gugudan.java
@@ -6,7 +6,11 @@ public class Gugudan {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		if(n==0) {
+		if(n<0) {
+			System.out.println("출력 불가");
+			return;
+		}
+		else if(n==0) {
 			for (int i = 2; i < 10; i++) {
 				for (int j = 1; j < 10; j++) {
 					System.out.print(i+"*"+j+"="+i*j+"\t");
